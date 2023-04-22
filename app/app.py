5a1bf8b5ec2263403539edef6afebda16108076f
marcelin-ne <marcela.montalvo@epn.edu.ec>
2023-04-21 23:40:18 -0500

    pass data thru collections, def for handle error , def before and after request , block creations and layout

--- app/app.py
+++ app/app.py
@@ -1,6 +1,18 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,request,redirect,url_for
 
 app=Flask(__name__)
+
+@app.before_request
+def before_request():
+    print('Antes de la peticion')
+    
+@app.after_request
+def after_request(response):
+    print('Despues de la peticion')
+    return response
+
+
+
 
 @app.route('/') #Ruta Raiz
 def index():  #Vista expresada en forma de funcion 
@@ -15,6 +27,30 @@ def index():  #Vista expresada en forma de funcion
     }
     return render_template('index.html',data=data)
 
+
+@app.route('/contacto/<nombre>/<int:edad>') #Ruta Raiz
+def contacto(nombre,edad):
+    data={
+        'titulo':'Contacto',
+        'nombre':nombre,
+        'edad':edad
+    }
+    return render_template ('contacto.html',data=data)
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return 'ok'
+
+def pagina_no_encontrada(error):
+    #return render_template('404.html'),404
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
+    app.add_url_rule('/query_string',view_func=query_string)
+    app.register_error_handler(404,pagina_no_encontrada)
     app.run(debug=True, port=5000)
     
